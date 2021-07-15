e8ca814019535789abd271c180c1e2ee75d38210
Sergio Furgeri <sfurgeri@fiecalunos.lan>
2021-07-15 16:37:51 -0300

    Inserção de imagens

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import C1 from './componentes/comp1' //componente1 importado
-// import C2 from './componentes/comp2' //componente2 importado
-// import C3 from './componentes/comp3' //componente3 importado
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Caixas from './componentes/caixas'
 
-// const exibir = (vp1) => {
-//   if(vp1 == true){
-//     return(<Text>Curso de React Native</Text>)
-//   }
-//   else{
-//     return(<Text> - - - </Text>)
-//   }
-// }
-
-//Componente de função
-//Com o componente externo
 export default function App() {
   let valorExibir = true
   return (
     <View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/favicon.png')}/>
       <Caixas exibir={valorExibir}/>
-      {/* <C1 curso='React Native' os='Android' cor='#f00'/> 
-      <C2 curso='Desenvolvimento Mobile' os='IOS' cor='#0ff'/>
-      <C3/> */}
       {/* Exibindo direto */}
       {valorExibir?(<Text>Curso de React Native</Text>):(<Text> - - - </Text>)}
       {/* Maneira simples de fazer uma condição, se for false nem vai fazer nada. Mas caso for true vai parar no começo */}
@@ -41,37 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 50,
+    resizeMode: 'contain'
+  }
 });
-
-//Componente de função
-//Sem o componente externo
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <View>
-//         <Text>Primeiro texto</Text>
-//         <StatusBar style="auto" />
-//       </View>
-//       <View>
-//         <Text>Segundo texto</Text>
-//         <StatusBar style="auto" />
-//       </View>
-//       <View>
-//         <Text>Terceiro texto</Text>
-//         <StatusBar style="auto" />
-//       </View>
-//     </View>
-//   );
-// }
-
-//Componente de classe
-// export default class App extends Component{
-//   render(){
-//     return(
-//       <View>
-//         <Text>AOOOOOBA</Text>
-//       </View>
-      
-//     );
-//   }
-// }
