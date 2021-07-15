37f865ca97eaf0622991437a8fba0df87b393ca6
Sergio Furgeri <sfurgeri@fiecalunos.lan>
2021-07-15 15:25:00 -0300

    Ainda aula com elementos flex

--- componentes/caixas.js
+++ componentes/caixas.js
@@ -3,10 +3,13 @@ import { View } from 'react-native'
 
 export default function() {
     return(
-       <View style={{width:'100%', height:300, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
-           <View style={{width:50,height:50,backgroundColor:'#00f'}}></View>
-           <View style={{width:50,height:50,backgroundColor:'#00a'}}></View>
-           <View style={{width:50,height:50,backgroundColor:'#005'}}></View>
+       <View style={{width:'100%', height:300, flexDirection:'column', justifyContent:'center', alignItems:'stretch'}}>
+           <View style={{flexGrow:1,backgroundColor:'#00f'}}></View>
+           <View style={{flexGrow:1,backgroundColor:'#00a'}}></View>
+           <View style={{flexGrow:1,backgroundColor:'#005'}}></View>
+           <View style={{flexGrow:1,backgroundColor:'#f00'}}></View>
+           <View style={{flexGrow:1,backgroundColor:'#a00'}}></View>
+           <View style={{flexGrow:1,backgroundColor:'#500'}}></View>
        </View>
     )
 }
@@ -14,3 +17,7 @@ export default function() {
 // flexDirection define o ponto de referência do elemento, se vai ser o main-axis ou o cross-axis
 // justifyContent alinha os elementos horizontalmente
 // alignItems alinha os elementos verticalmente
+
+// quando o elemento não tem tamanho, o flexDirection está column e o alignItems está stretch, o elemento se esticará
+// o flexWrap define se o elemento que não está cabendo dentro do elemento pai, quebrará linha ou não
+// flexGrow 1obra o tamanho e reajusta todos os elementos
