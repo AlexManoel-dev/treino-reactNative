1b7b60ed0e7f72d543a621d448361a03f43714a1
Sergio Furgeri <sfurgeri@fiecalunos.lan>
2021-07-15 17:17:44 -0300

    Usando Button junto com Alert no onPress

--- App.js
+++ App.js
@@ -1,35 +1,17 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
+// import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
-import Caixas from './componentes/caixas'
-
-/* 
-(n) => {return n*2}
-(n1,n2) => {return n1 + n2}
-
-somente se pode fazer assim caso houver somente 1 parâmetro
-n => {
-  return n*2
-}
-*/
-
-const dobro = n => n*2
-
-const soma = (n1,n2) => n1 + n2
 
 export default function App() {
-  let valorExibir = true
   return (
     <View style={styles.container}>
-      <Text>{dobro(2)}</Text>
-      <Text>{soma(2,4)}</Text>
       <Image style={styles.logo} source={require('./assets/favicon.png')}/>
-      <Caixas exibir={valorExibir}/>
-      {/* Exibindo direto */}
-      {valorExibir?(<Text>Curso de React Native</Text>):(<Text> - - - </Text>)}
-      {/* Maneira simples de fazer uma condição, se for false nem vai fazer nada. Mas caso for true vai parar no começo */}
-      {valorExibir && (<Text>Curso de React Native</Text>)}
-      {/* {exibir(true)} */}
+      <Button 
+        title='Mostrar Mensagem'
+        onPress={() => {
+          Alert.alert('Mensagem','Isso é um alerta!')
+        }}
+      />
     </View>
   );
 }
